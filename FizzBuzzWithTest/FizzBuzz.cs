676ab2749f6af7b63105ec93fabb851c15b6abf2
x0rld <git@x0rld.eu>
2022-04-16 19:45:35 +0200

    do the test for fizz and buzz simply

--- FizzBuzzWithTest/FizzBuzz.cs
+++ FizzBuzzWithTest/FizzBuzz.cs
@@ -4,6 +4,14 @@ public static class FizzBuzz
 {
     public static string ToFizzBuzz(int i)
     {
+        if (i == 3)
+        {
+            return "fizz";
+        }
+        if (i == 5)
+        {
+            return "buzz";
+        }
         return "1";
     }
 }
--- FizzBuzzWithTest/FizzBuzzTest.cs
+++ FizzBuzzWithTest/FizzBuzzTest.cs
@@ -8,7 +8,20 @@ public class FizzBuzzTest
     [InlineData(1)]
     public void one_should_be_one(int value)
     {
-        Assert.True(FizzBuzz.ToFizzBuzz(value) == "1");
+        Assert.Equal("1", FizzBuzz.ToFizzBuzz(value));
+    }
+    
+    [Theory]
+    [InlineData(3)]
+    public void three_should_be_fizz(int value)
+    {
+        Assert.Equal("fizz",FizzBuzz.ToFizzBuzz(value));
     }
 
+    [Theory]
+    [InlineData(5)]
+    public void five_should_be_buzz(int value)
+    {
+        Assert.Equal("buzz", FizzBuzz.ToFizzBuzz(value));
+    }
 }
